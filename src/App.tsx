bb0a07e3c589be938556077410cde89833ec494a
Levi <47727379+lbare@users.noreply.github.com>
2025-04-10 13:47:54 -0700

    Resizing to help with responsiveness

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,34 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import "./App.css";
 import TVFrame from "./components/TVFrame";
-import NavBar from "./components/NavBar";
 import TVContent from "./components/TVContent";
 import BottomIcons from "./components/LinkIcons";
 
 function App() {
-  const [currentProject, setCurrentProject] = useState<string>("about");
-  useEffect(() => {
-    console.log(`Current project: ${currentProject}`);
-  }, [currentProject]);
+  const [currentProject, setCurrentProject] = useState<number>(0);
+  const [tvOn, setTvOn] = useState<boolean>(true);
 
   return (
-    <div className="relative h-screen w-screen">
-      <TVFrame />
-      <NavBar
-        currentProject={currentProject}
-        setCurrentProject={setCurrentProject}
-      />
-      <TVContent currentProject={currentProject} />
-      <BottomIcons />
+    <div className="flex items-end justify-center h-screen w-screen bg-white">
+      <div
+        className="
+          relative       /* so children can be absolutely positioned */
+          w-full
+          max-w-3/5 /* or any max width you like */
+          aspect-[4/3]   /* keep the TV in a 4:3 ratio (optional) */
+          flex items-center justify-center border
+        "
+      >
+        <TVFrame
+          currentProject={currentProject}
+          setCurrentProject={setCurrentProject}
+          tvOn={tvOn}
+          setTvOn={setTvOn}
+        />
+
+        <TVContent currentProject={currentProject} />
+        {/* <BottomIcons /> */}
+      </div>
     </div>
   );
 }
--- src/components/TVContent.tsx
+++ src/components/TVContent.tsx
@@ -11,23 +11,20 @@ interface TVContentProps {
   currentProject: string;
 }
 
-const projectComponents: Record<string, JSX.Element> = {
-  about: <About />,
-  alderGreens: <AlderGreens />,
-  baseballStats: <BaseballStats />,
-  uhub: <UHub />,
-  photoTracker: <PhotoTracker />,
-  messenger: <Messenger />,
-  weatherApp: <WeatherApp />,
-};
+const projectComponents: Record<string, JSX.Element> = [
+  <About />,
+  <UHub />,
+  <AlderGreens />,
+  <Messenger />,
+  <PhotoTracker />,
+  <BaseballStats />,
+  <WeatherApp />,
+];
 
 const TVContent: React.FC<TVContentProps> = ({ currentProject }) => {
   return (
     <div
-      className="absolute overflow-hidden z-10 
-                  lg:top-[15.5%] lg:left-[25.5%] lg:w-[39%] lg:h-[52.5%]
-                  sm:top-[20%] sm:left-[25%] sm:w-[60%] sm:h-[40%]
-                  md:top-[15.5%] md:left-[25.5%] md:w-[51%] md:h-[45%]"
+      className="absolute overflow-hidden z-0 top-[6%] left-[13%] w-[58%] h-[58%]"
     >
       {projectComponents[currentProject] || <About />}
     </div>
