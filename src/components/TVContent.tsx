9c93320b147e0ee084dbc204eaeaf45ee77e67e6
Levi <47727379+lbare@users.noreply.github.com>
2025-04-11 13:05:40 -0700

    Change mobile tv content positioning

--- src/components/TVContent.tsx
+++ src/components/TVContent.tsx
@@ -6,6 +6,7 @@ import UHub from "../projects/UHub";
 import PhotoTracker from "../projects/PhotoTracker";
 import Messenger from "../projects/Messenger";
 import WeatherApp from "../projects/WeatherApp";
+import { isMobile } from "react-device-detect";
 
 interface TVContentProps {
   currentProject: number;
@@ -27,6 +28,14 @@ const TVContent: React.FC<TVContentProps> = ({
   fullScreen,
 }) => {
   const SelectedProject = projectComponents[currentProject] || About;
+
+  if (isMobile) {
+    return (
+      <div className="absolute overflow-hidden z-0 top-[29%] left-[32%] w-[29%] h-[29%]">
+        <SelectedProject fullScreen={fullScreen} />
+      </div>
+    );
+  }
 
   return (
     <div className="absolute overflow-hidden z-0 top-[6%] left-[13%] w-[58.5%] h-[58%]">
