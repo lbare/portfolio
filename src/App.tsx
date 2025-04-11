a97c5a5c4243aa5205c05d583a11bd233fa4610f
Levi <47727379+lbare@users.noreply.github.com>
2025-04-10 20:13:37 -0700

    Move assets

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,18 @@ import { useState } from "react";
 import "./App.css";
 import TVFrame from "./components/TVFrame";
 import TVContent from "./components/TVContent";
-import BottomIcons from "./components/LinkIcons";
 
 function App() {
   const [currentProject, setCurrentProject] = useState<number>(0);
   const [tvOn, setTvOn] = useState<boolean>(true);
 
   return (
-    <div className="flex items-end justify-center h-screen w-screen bg-[url('/src/assets/lines.svg')] bg-auto bg-no-repeat bg-center">
+    <div className="flex items-center justify-center h-screen w-screen bg-[url('/src/assets/svg/lines.svg')] bg-auto bg-no-repeat bg-center pt-6">
       <div
         className="
           relative       /* so children can be absolutely positioned */
-          w-full
-          max-w-8/12 /* or any max width you like */
+          h-full
+          max-w-fulls /* or any max width you like */
           aspect-[4/3]   /* keep the TV in a 4:3 ratio (optional) */
           flex items-center justify-center
         "
@@ -25,9 +24,7 @@ function App() {
           tvOn={tvOn}
           setTvOn={setTvOn}
         />
-
         <TVContent currentProject={currentProject} />
-        {/* <BottomIcons /> */}
       </div>
     </div>
   );
