dd7e267e6d2762d8e8c4c2bf668a46e98cbaf0ee
Levi <47727379+lbare@users.noreply.github.com>
2025-04-15 16:59:32 -0700

    Add spinner, fix weird mobile bug

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,9 @@ import "./App.css";
 import TVFrame from "./components/TVFrame";
 import TVContent from "./components/TVContent";
 import TVFrameMobile from "./components/TVFrameMobile";
-import { isMobile } from "react-device-detect";
+import { isMobileOnly, isTablet } from "react-device-detect";
 import usePreloadImages from "./hooks/usePreloadImages";
+import { ColorRing } from "react-loader-spinner";
 
 function App() {
   const [currentProject, setCurrentProject] = useState<number>(0);
@@ -13,6 +14,8 @@ function App() {
   const [shouldShake, setShouldShake] = useState<boolean>(false);
   const [powerOffAnimating, setPowerOffAnimating] = useState<boolean>(false);
   const firstRender = useRef(true);
+  const images = usePreloadImages();
+  const [isReady, setIsReady] = useState<boolean>(false);
 
   useEffect(() => {
     if (firstRender.current) {
@@ -35,20 +38,39 @@ function App() {
     }
   }, [tvOn]);
 
-  usePreloadImages();
+  useEffect(() => {
+    setTimeout(() => {
+      setIsReady(true);
+    }, 1500);
+  }, []);
 
-  if (isMobile) {
+  if (!isReady || !images) {
+    return (
+      <div className="flex items-center justify-center h-dvh w-screen bg-black overflow-hidden">
+        <ColorRing
+          visible={true}
+          height="80"
+          width="80"
+          ariaLabel="color-ring-loading"
+          wrapperStyle={{}}
+          wrapperClass="color-ring-wrapper"
+          colors={["#D25C24", "#F79633", "#FEB256", "#EF7521", "#CF7A24FF"]}
+        />
+      </div>
+    );
+  }
+  if (isMobileOnly) {
     return (
       <div
-        className={`flex items-center justify-center h-screen w-screen bg-[url('/src/assets/svg/lines.svg')] grayscale-100 brightness-50 transition-all duration-1000 ease-out ${
+        className={`flex items-center justify-center h-dvh w-screen bg-[url('/src/assets/svg/lines.svg')] grayscale-100 brightness-50 transition-all duration-1000 ease-out overflow-hidden ${
           tvOn && "filter-none"
         }
-        bg-auto bg-no-repeat bg-center`}
+          bg-auto bg-no-repeat bg-center`}
       >
         <div
           className={`relative h-full aspect-[4/3] flex items-center justify-center mb-20
-            ${shouldShake ? "tv-shake" : ""}
-            `}
+              ${shouldShake ? "tv-shake" : ""}
+              `}
         >
           <TVFrameMobile
             currentProject={currentProject}
@@ -71,9 +93,40 @@ function App() {
     );
   }
 
+  if (isTablet) {
+    return (
+      <div
+        className={`flex items-center justify-center h-dvh w-screen bg-[url('/src/assets/svg/lines.svg')] grayscale-100 brightness-50 transition-all duration-500 ease-out ${
+          tvOn && "filter-none"
+        } "bg-auto bg-no-repeat bg-center pt-6`}
+      >
+        <div
+          className={`relative h-full aspect-[4/3] flex items-center justify-center ${
+            shouldShake ? "tv-shake" : ""
+          }`}
+        >
+          <TVFrame
+            currentProject={currentProject}
+            setCurrentProject={setCurrentProject}
+            tvOn={tvOn}
+            setTvOn={setTvOn}
+            fullScreen={fullScreen}
+            setFullScreen={setFullScreen}
+          />
+          <TVContent
+            currentProject={currentProject}
+            fullScreen={fullScreen}
+            tvOn={tvOn}
+          />
+          {powerOffAnimating && <div className="power-off-glow z-[60]" />}
+        </div>
+      </div>
+    );
+  }
+
   return (
     <div
-      className={`flex items-center justify-center h-screen w-screen bg-[url('/src/assets/svg/lines.svg')] grayscale-100 brightness-50 transition-all duration-500 ease-out ${
+      className={`flex items-center justify-center h-dvh w-screen bg-[url('/src/assets/svg/lines.svg')] grayscale-100 brightness-50 transition-all duration-500 ease-out ${
         tvOn && "filter-none"
       } "bg-auto bg-no-repeat bg-center pt-6`}
     >
